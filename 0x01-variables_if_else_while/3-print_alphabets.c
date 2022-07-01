602532ba20b983adac69d8c03248e4371259e75f
kaleab <akaleab.9363@gmail.com>
2022-07-01 22:28:13 +0300

    2

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <iostream>
+
+/**
+ * main - print lowercase alphabet followed by uppercase letters
+ * Return: 0 if ran correctly
+ */
+
+int main (void)
+
+	int x;
+
+	int y;
+
+	for (x = 97; x <= 122; x++)
+		putchar(x);
+	for (y = 67; y<= 90; y++)
+		putchar(y)
+	putchar('\n');
+	return (0);
