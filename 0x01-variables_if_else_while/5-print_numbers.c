ff185b5ba0a8a4e0fb2526dbfb6774a6ce9f6033
kaleab <akaleab.9363@gmail.com>
2022-07-01 22:58:53 +0300

    5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - print single digits
+ * Return: 0 if ran correctly
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = 0; x <= 0; x++)
+		printf("%d/n", x);
+	return (0);
+}
