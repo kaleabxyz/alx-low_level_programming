539341dacd19313d34ac05d40de39e133eba8282
kaleab <akaleab.9363@gmail.com>
2022-07-01 21:38:01 +0300

    1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -13,6 +13,7 @@ int main(void)
 	int n;
 
 	int m;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
@@ -23,7 +24,7 @@ int main(void)
 		printf("%s\n", " and is greater than 5");
 		else if (m < 5)
 			printf("%s\n", " and is less than 6 and not 0");
-	else
-		printf("%s\n", " and is 0");
+		else if (m == 0)
+			printf("%s\n", " and is 0");
 	return (0);
 }
