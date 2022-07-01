a299f465d44fe649e118d932b14e0b32e182fc45
kaleab <akaleab.9363@gmail.com>
2022-07-01 22:52:23 +0300

    4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -10,9 +10,8 @@ int main(void)
 	int x;
 
 	for (x = 97; x <= 122; x++)
-
-	{	if (x != 113 && x != 101)
-		putchar(x);
+		if (x != 113 && x != 101)
+		{	putchar(x);
 	}
 	putchar ('\n');
 	return (0);
