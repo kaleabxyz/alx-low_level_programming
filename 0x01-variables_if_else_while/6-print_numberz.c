e80af06100bb2d0e5ab255006be97b9348c55f3c
kaleab <akaleab.9363@gmail.com>
2022-07-04 20:05:23 +0300

    6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - print numberz
+ * return: 0 if successfully ran
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = 0; x < 10; x++)
+		putchar (x);
+	putchar (" ");
+	return (0);
+}
