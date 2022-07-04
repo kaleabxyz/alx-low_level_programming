5fdbdf72fdf71a1556b5b0779883fc20c2bf32f9
kaleab <akaleab.9363@gmail.com>
2022-07-04 21:30:16 +0300

    8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - print numberz
+ * Return: 0 if successfully ran
+ */
+
+int main(void)
+{
+	int x;
+
+	int y;
+
+	for (x = 48; x <= 57; x++)
+		putchar(x);
+	for (y = 97; y <= 102; y++)
+		putchar(y);
+	putchar('\n');
+	return (0);
