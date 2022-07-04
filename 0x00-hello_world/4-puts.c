4b98d6403b343b457ce7d448a949e09184d3e9dc
kaleab <akaleab.9363@gmail.com>
2022-07-04 22:51:39 +0300

    4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - print
+ * Return:  0 if successfull
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
+Footer
+
