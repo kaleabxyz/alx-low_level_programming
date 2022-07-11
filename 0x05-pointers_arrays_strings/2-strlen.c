6a29464bf60cc5e95e74c4a2a2d1737c016656a4
kaleab <akaleab.9363@gmail.com>
2022-07-11 22:11:40 +0300

    1

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "HEADER.H"
+
+/**
+ * _strlen - return the length of a string
+ * @s: variable
+ * Return: the length
+ */
+
+int _strlen(char *s)
+{
+	int n;
+
+	for (n = 0; s[1] != '/0'; i++)
+		continue;
+	return (n);
+}
